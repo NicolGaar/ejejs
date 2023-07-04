bc29c515825b108709e40f8868b5dee3e5973b81
nicogaar <nicol.gaar@gmail.com>
2023-07-04 10:24:41 -0400

    mas ejercicios

---
+++ Fetch/fetch.js
@@ -0,0 +1,15 @@
+//funcion para consultar los datos
+
+
+fetch('https://reqres.in/api/users')
+.then(response => response.json())
+.then(json => console.log(json))
+
+
+
+
+
+//inyectar datoa 
+function addUser () {
+
+}
